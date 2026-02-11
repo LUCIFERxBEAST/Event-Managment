eb3c01acd93c3f0fed4bfe4cd0bfdb2fa71b7d22
LUCIFERxBEAST <abhayyyrawatart@gmail.com>
2026-02-11 12:38:56 +0530

    Initial Commit

--- config/db.php
+++ config/db.php
@@ -1,15 +1,19 @@
 <?php
-/* FILE: config/db.php */
+// FILE: config/db.php
 
+// Vercel uses standard environment variables
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT') ?: 3306; // Default to 3306 if not set
 
-// Create Connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Check Connection
 if ($conn->connect_error) {
+    // Ideally, don't show specific errors to users in production
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Set Timezone
 date_default_timezone_set('Asia/Kolkata');
 ?>
